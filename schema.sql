59d568a0a175f73d5deb3763640f2087fca1903a
Paweł Bednarz <pbedn@outlook.com>
2017-05-15 22:13:46 +0200

    Type in schema

--- schema.sql
+++ schema.sql
@@ -7,9 +7,9 @@ create sequence trains.gtfs_id_seq
 create table trains.gtfs
 (
     id integer default nextval('trains.gtfs_id_seq'::regclass) not null
-        constraint gtfs_pkey primary key,
-    route char(6) not null,
-    active boolean default true not null,
+        constraint gtfs_pkey primary key
+    ,route char(6) not null
+    ,active boolean default true not null
 );
 
 create index gtfs_id_idx
